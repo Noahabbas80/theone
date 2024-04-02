a88604d45a0d15c952cee4aa87765a3491527a57
noaha90 <noahabb@outlook.com>
2024-04-02 07:48:17 -0400

    rwe
    rwe

--- routes/index.js
+++ routes/index.js
@@ -84,7 +84,7 @@ let trivia = [
     a: `combat update`,
     b: 'nether update',
     c: 'village & pillage update',
-    d: 'combat update',
+    d: 'flight update',
     answer: 'b'
 },  
 {
@@ -96,7 +96,7 @@ let trivia = [
     answer: 'c'
 },  
 {
-    question: `an elementary charge is equal to 10¹⁶ times...`,
+    question: `an elementary charge is equal to 10^-19 times...`,
     a: `7.53`,
     b: '1.32',
     c: '1.60',
@@ -144,7 +144,7 @@ let trivia = [
   answer: 'b'
 },
 {
-  question: `Which one of these Noahs is not currently a sophmore as of 2023-2024`,
+  question: `Which one of these Noahs is not currently a sophomore as of 2023-2024`,
   a: 'Noah Abbas',
   b: 'Noah Boyle',
   c: 'Noah Rozin',
@@ -215,6 +215,126 @@ let trivia = [
   d: 'The Fetch Pledge',
   answer: 'a'
 },
+{
+  question: `How many men reign as the king of Scotland throughout the play "MacBeth"`,
+  a: '1',
+  b: '2',
+  c: '3',
+  d: '4',
+  answer: 'c'
+},
+{
+  question: `Which keyword is NOT a valid way of declaring a variable in JavaScript`,
+  a: 'let',
+  b: 'var',
+  c: 'type',
+  d: 'const',
+  answer: 'c'
+},
+{
+  question: `The capital of Iowa is... `,
+  a: 'Des Moines',
+  b: 'New York',
+  c: 'Helena',
+  d: 'Lansing',
+  answer: 'a'
+},  
+{
+  question: `On what day did Shrek 2 come out`,
+  a: 'May 19, 2004',
+  b: 'April 22, 2001',
+  c: 'August 11, 2001',
+  d: 'July 4, 2002',
+  answer: 'b'
+},  
+{
+  question: `Which one of these acronyms are not a type of 3D printing filament`,
+  a: 'PLA',
+  b: 'IQW',
+  c: 'HIPS',
+  d: 'ABS',
+  answer: 'b'
+},
+{
+  question: `Who was the author of the classic novel "The Scarlet Letter"`,
+  a: 'Nathaniel Hawthorne',
+  b: 'Pierce Flits',
+  c: 'J. D. Salinger',
+  d: 'Noah Rozin',
+  answer: 'a'
+},
+{
+  question: `Who was the author of the classic novel "The Scarlet Letter"`,
+  a: 'Nathaniel Hawthorne',
+  b: 'Pierce Flits',
+  c: 'J. D. Salinger',
+  d: 'Noah Rozin',
+  answer: 'a'
+},
+{
+  question: `Who was the author of the classic novel "The Scarlet Letter"`,
+  a: 'Nathaniel Hawthorne',
+  b: 'Pierce Flits',
+  c: 'J. D. Salinger',
+  d: 'Noah Rozin',
+  answer: 'a'
+},
+{
+  question: `In "Gravity Falls", where does Dipper find the third journal`,
+  a: 'Inside the Sunchaser',
+  b: 'Behind the Mystery Shack',
+  c: 'In the gnome forest',
+  d: 'In a metal tree',
+  answer: 'd'
+},
+{
+  question: `What is the full name of the main character in the Bee Movie`,
+  a: 'Barry Billop Bartson the Second',
+  b: 'Barry Jerry Seinfeld ',
+  c: 'Barry Bailey Benson',
+  d: 'Bartholomew Bailey "Barry" Benson ',
+  answer: 'd'
+},
+{
+  question: `What is the slogan written on Ken's tie-die hoodie in the Barbie movie`,
+  a: `It's Oken`,
+  b: `Ken't Go On`,
+  c: 'I Am Kenough',
+  d: 'Ken the Ten',
+  answer: 'c'
+},
+{
+  question: `What Ivy League has a section """named""" after a physics teacher in SITHS `,
+  a: `Harvard`,
+  b: `UPenn`,
+  c: 'Brown',
+  d: 'Cornell',
+  answer: 'b'
+},
+{
+  question: `Which one of these characters is or will not be played by Chris Pratt`,
+  a: `Garfield`,
+  b: `Owen Grady`,
+  c: 'Andy Dwyer',
+  d: 'Peter Pan',
+  answer: 'd'
+},
+{
+  question: `What is the meaning of life the universe and everything`,
+  a: `Sleep`,
+  b: `Memories`,
+  c: 'Dreams',
+  d: '42',
+  answer: 'd'
+},
+{
+  question: `What was the name of Leonardo Da Vinci's Teacher?`,
+  a: `Joseph Fourier`,
+  b: `Andrea del Verrocchio`,
+  c: 'Daniel Bernoulli',
+  d: 'Keith Devlin',
+  answer: 'b'
+},
 ]
 
 
@@ -240,350 +360,350 @@ let trivia = [
 
 let data = [
     {
-      question: 'What percentage of sophmores own an Android phone',
+      question: 'What percentage of sophomores own an Android phone',
       yes: 20,
       no: 64,
       percent: 24
     },
     {
-      question: 'What percentage of sophmores listen to Taylor Swift on a regular basis',
+      question: 'What percentage of sophomores listen to Taylor Swift on a regular basis',
       yes: 11,
       no: 73,
       percent: 13
     },
     {
-      question: 'What percentage of sophmores have a favorite subject',
+      question: 'What percentage of sophomores have a favorite subject',
       yes: 60,
       no: 22,
       percent: 73
     },
     {
-      question: 'What percentage of sophmores play either Genshin Impact or Honkai Star Rail',
+      question: 'What percentage of sophomores play either Genshin Impact or Honkai Star Rail',
       yes: 12,
       no: 71,
       percent: 14
     },
     {
-      question: 'What percentage of sophmores prefer watching movies at home instead of at a theater',
+      question: 'What percentage of sophomores prefer watching movies at home instead of at a theater',
       yes: 45,
       no: 32,
       percent: 58
     },
     {
-      question: 'What percentage of sophmores have beaten a Pokemon game',
+      question: 'What percentage of sophomores have beaten a Pokemon game',
       yes: 27,
       no: 51,
       percent: 35
     },
     {
-      question: 'What percentage of sophmores watch anime on a regular basis',
+      question: 'What percentage of sophomores watch anime on a regular basis',
       yes: 10,
       no: 73,
       percent: 12
     },
     {
-      question: 'What percentage of sophmores think that they could beat a horse in a fight',
+      question: 'What percentage of sophomores think that they could beat a horse in a fight',
       yes: 28,
       no: 54,
       percent: 34
     },
     {
-      question: 'What percentage of sophmores prefer Xbox over PlayStation',
+      question: 'What percentage of sophomores prefer Xbox over PlayStation',
       yes: 21,
       no: 39,
       percent: 35
     },
     {
-      question: 'What percentage of sophmores prefer sweet foods over sour foods',
+      question: 'What percentage of sophomores prefer sweet foods over sour foods',
       yes: 56,
       no: 20,
       percent: 74
     },
     {
-      question: 'What percentage of sophmores have went to that one halal food cart near the school (you know the one)',
+      question: 'What percentage of sophomores have went to that one halal food cart near the school (you know the one)',
       yes: 51,
       no: 43,
       percent: 54
     },
     {
-      question: 'What percentage of sophmores have fully watched the hit NBC show "The Office"',
+      question: 'What percentage of sophomores have fully watched the hit NBC show "The Office"',
       yes: 13,
       no: 69,
       percent: 16
     },
     {
-      question: 'What percentage of sophmores have been to the store "Something Sweet"',
+      question: 'What percentage of sophomores have been to the store "Something Sweet"',
       yes: 59,
       no: 23,
       percent: 72
     },
     {
-      question: 'What percentage of sophmores have missed their first period due to sleeping in',
+      question: 'What percentage of sophomores have missed their first period due to sleeping in',
       yes: 22,
       no: 61,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores think that chicken tenders are the best school lunch',
+      question: 'What percentage of sophomores think that chicken tenders are the best school lunch',
       yes: 25,
       no: 55,
       percent: 31
     },
     {
-      question: 'What percentage of sophmores are on a sports team',
+      question: 'What percentage of sophomores are on a sports team',
       yes: 36,
       no: 48,
       percent: 43
     },
     {
-      question: 'What percentage of sophmores have been to the Wingstop on Hylan Boulevard',
+      question: 'What percentage of sophomores have been to the Wingstop on Hylan Boulevard',
       yes: 28,
       no: 56,
       percent: 33
     },
     {
-      question: 'What percentage of sophmores pour milk before cereal',
+      question: 'What percentage of sophomores pour milk before cereal',
       yes: 22,
       no: 61,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores have a favorite teacher in SITHS',
+      question: 'What percentage of sophomores have a favorite teacher in SITHS',
       yes: 71,
       no: 12,
       percent: 86
     },
     {
-      question: 'What percentage of sophmores, if accepted into every college, would go to an Ivy League',
+      question: 'What percentage of sophomores, if accepted into every college, would go to an Ivy League',
       yes: 51,
       no: 26,
       percent: 66
     },
     {
-      question: 'What percentage of sophmores wake up before 6AM on a regular basis',
+      question: 'What percentage of sophomores wake up before 6AM on a regular basis',
       yes: 23,
       no: 61,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores sleep after 1AM on a regular basis',
+      question: 'What percentage of sophomores sleep after 1AM on a regular basis',
       yes: 22,
       no: 62,
       percent: 26
     },
     {
-      question: 'What percentage of sophmores like sushi as their favorite food',
+      question: 'What percentage of sophomores like sushi as their favorite food',
       yes: 21,
       no: 62,
       percent: 25
     },
     {
-      question: 'What percentage of sophmores usually get two or more toppings on pizza',
+      question: 'What percentage of sophomores usually get two or more toppings on pizza',
       yes: 20,
       no: 63,
       percent: 24
     },
     {
-      question: 'What percentage of sophmores intend on taking AP Physics 2 in their Junior Year',
+      question: 'What percentage of sophomores intend on taking AP Physics 2 in their Junior Year',
       yes: 22,
       no: 59,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores are pushing their art credit to their junior or senior year',
+      question: 'What percentage of sophomores are pushing their art credit to their junior or senior year',
       yes: 11,
       no: 71,
       percent: 13
     },
     //nte
     {
-      question: 'What percentage of sophmores plan in advance for April Fools Day',
+      question: 'What percentage of sophomores plan in advance for April Fools Day',
       yes: 18,
       no: 64,
       percent: 22
     },
     {
-      question: 'What percentage of sophmores would consider LeBron James as "The Goat"',
+      question: 'What percentage of sophomores would consider LeBron James as "The Goat"',
       yes: 24,
       no: 42,
       percent: 36
     },
     {
-      question: 'What percentage of sophmores would consider themselves to have bad luck on average',
+      question: 'What percentage of sophomores would consider themselves to have bad luck on average',
       yes: 29,
       no: 45,
       percent: 39
     },
     {
-      question: 'What percentage of sophmores prefer to go by their last or middle name',
+      question: 'What percentage of sophomores prefer to go by their last or middle name',
       yes: 8,
       no: 64,
       percent: 11
     },
     {
-      question: 'What percentage of sophmores have a least favorite movie',
+      question: 'What percentage of sophomores have a least favorite movie',
       yes: 22,
       no: 61,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores have pulled an all-nighter',
+      question: 'What percentage of sophomores have pulled an all-nighter',
       yes: 50,
       no: 31,
       percent: 62
     },
     {
-      question: 'What percentage of sophmores have been to all five boroughs in NYC',
+      question: 'What percentage of sophomores have been to all five boroughs in NYC',
       yes: 64,
       no: 19,
       percent: 77
     },
     {
-      question: 'What percentage of sophmores have been inside a Bass Pro shop',
+      question: 'What percentage of sophomores have been inside a Bass Pro shop',
       yes: 18,
       no: 63,
       percent: 22
     },
     {
-      question: 'What percentage of sophmores have written a literature review',
+      question: 'What percentage of sophomores have written a literature review',
       yes: 40,
       no: 37,
       percent: 52
     },
     {
-      question: 'What percentage of sophmores have signed up for a club and never attended a meeting',
+      question: 'What percentage of sophomores have signed up for a club and never attended a meeting',
       yes: 70,
       no: 12,
       percent: 85
     },
     {
-      question: 'What percentage of sophmores have done a homework assignment in class and got a 100 on it',
+      question: 'What percentage of sophomores have done a homework assignment in class and got a 100 on it',
       yes: 79,
       no: 4,
       percent: 95
     },
     {
-      question: 'What percentage of sophmores play rhythm games while NOT knowing how to play an instrument',
+      question: 'What percentage of sophomores play rhythm games while NOT knowing how to play an instrument',
       yes: 22,
       no: 61,
       percent: 27
     },
     {
-      question: 'What percentage of sophmores have had to had their school ID replaced after losing it',
+      question: 'What percentage of sophomores have had to had their school ID replaced after losing it',
       yes: 39,
       no: 44,
       percent: 47
     },
     {
-      question: 'What percentage of sophmores take the S79 bus home on a frequent basis',
+      question: 'What percentage of sophomores take the S79 bus home on a frequent basis',
       yes: 53,
       no: 30,
       percent: 64
     },
     {
-      question: 'What percentage of sophmores prefer indie video games compared to triple A games',
+      question: 'What percentage of sophomores prefer indie video games compared to triple A games',
       yes: 36,
       no: 25,
       percent: 59
     },
     {
-      question: 'What percentage of sophmores prefer the name "X" over "Twitter" ',
+      question: 'What percentage of sophomores prefer the name "X" over "Twitter" ',
       yes: 4,
       no: 74,
       percent: 5
     },
     {
-      question: 'What percentage of sophmores own more than one SITHS t-shirt',
+      question: 'What percentage of sophomores own more than one SITHS t-shirt',
       yes: 60,
       no: 21,
       percent: 74
     },
     {
-      question: 'What percentage of sophmores have more than 3 streaming subscriptions',
+      question: 'What percentage of sophomores have more than 3 streaming subscriptions',
       yes: 25,
       no: 56,
       percent: 31
     },
     {
-      question: 'What percentage of sophmores have been to a Panera Bread',
+      question: 'What percentage of sophomores have been to a Panera Bread',
       yes: 47,
       no: 36,
       percent: 57
     },
     {
-      question: 'What percentage of sophmores pack their own lunches to school',
+      question: 'What percentage of sophomores pack their own lunches to school',
       yes: 27,
       no: 52,
       percent: 34
     },
     {
-      question: 'What percentage of sophmores want to graduate with 8 or more AP classes',
+      question: 'What percentage of sophomores want to graduate with 8 or more AP classes',
       yes: 55,
       no: 19,
       percent: 74
     },
     {
-      question: 'What percentage of sophmores have watched a show or movie with Chris Pratt in it in the past year',
+      question: 'What percentage of sophomores have watched a show or movie with Chris Pratt in it in the past year',
       yes: 40,
       no: 39,
       percent: 51
     },
     {
-      question: 'What percentage of sophmores prefer earbuds over headphones',
+      question: 'What percentage of sophomores prefer earbuds over headphones',
       yes: 40,
       no: 35,
       percent: 53
     },
     {
-      question: 'What percentage of sophmores have a Snapchat',
+      question: 'What percentage of sophomores have a Snapchat',
       yes: 43,
       no: 40,
       percent: 52
     },
     {
-      question: 'What percentage of sophmores intend on taking Calc BC junior year',
+      question: 'What percentage of sophomores intend on taking Calc BC junior year',
       yes: 31,
       no: 48,
       percent: 39
     },
     {
-      question: 'What percentage of sophmores think math is red',
+      question: 'What percentage of sophomores think math is red',
       yes: 22,
       no: 55,
       percent: 29
     },
     {
-      question: 'What percentage of sophmores use light mode',
+      question: 'What percentage of sophomores use light mode',
       yes: 14,
       no: 64,
       percent: 18
     },
     {
-      question: 'What percentage of sophmores went to Christa Mcauliffe for middle school',
+      question: 'What percentage of sophomores went to Christa Mcauliffe for middle school',
       yes: 9,
       no: 69,
       percent: 12
     },
     {
-      question: 'What percentage of sophmores have coded in Scratch',
+      question: 'What percentage of sophomores have coded in Scratch',
       yes: 69,
       no: 12,
       percent: 85
     },
     {
-      question: 'What percentage of sophmores, if only able to pick one, would pick boba over coffee',
+      question: 'What percentage of sophomores, if only able to pick one, would pick boba over coffee',
       yes: 53,
       no: 25,
       percent: 68
     },
     {
-      question: 'What percentage of sophmores have worked in SING',
+      question: 'What percentage of sophomores have worked in SING',
       yes: 26,
       no: 57,
       percent: 31
     },
     {
-      question: 'What percentage of sophmores have yelled at friends/family when playing a board game',
+      question: 'What percentage of sophomores have yelled at friends/family when playing a board game',
       yes: 71,
       no: 9,
       percent: 89
@@ -655,9 +775,5 @@ function getParam(res,req){
     if(req.params.game == 'dt'){
       res.setHeader("Access-Control-Allow-Origin", "*");
         res.json(data[req.params.id ])
-    }
-    if(req.params.game == 'ql'){
-      res.setHeader("Access-Control-Allow-Origin", "*");
-        res.json({prompt: funnies[req.params.id]})
     }
 }
